9a0e2542a99bbd71bfc663ed1ea9b745c6b6404b
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-05-07 10:01:56 -0400

    Creates specs for checking ship locations, add basic logic to support test specs

---
+++ test/ship_test.js
@@ -0,0 +1,63 @@
+var expect = require('chai').expect;
+
+describe('check for ship', function(){
+  var checkForShip = require('../game_logic/ship_methods').checkForShip;
+  it('should correctly report no ship at a given players coordinates aka miss', function(){
+    var player = {
+      ships: [
+        {
+          locations: [[0, 0]]
+        }
+      ]
+    };
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
+
+  it('should correctly report a ship is at a given players coordinates aka hit', function(){
+    var player = {
+      ships: [
+        {
+          locations: [[0, 0]]
+        }
+      ]
+    };
+    expect(checkForShip(player, [0, 0])).to.be.true;
+  });
+
+  it('should handle ships located at more than one coordinate', function(){
+    var player = {
+      ships: [
+        {
+          locations: [[0, 0], [0, 1]]
+        }
+      ]
+    };
+    // mutliple expects not always a good idea because it may not be clear what a test is proving
+    expect(checkForShip(player, [0, 0])).to.be.true;
+    expect(checkForShip(player, [0, 1])).to.be.true;
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
+
+  it('should handle checking multiple ships', function(){
+    var player = {
+      ships: [
+        {
+          locations: [[0, 0], [0, 1]]
+        },
+        {
+          locations: [[1, 0], [1, 1]]
+        },
+        {
+          locations: [[2, 0], [2, 1], [2, 2], [2, 3]]
+        }
+      ]
+    };
+    // mutliple expects not always a good idea because it may not be clear what a test is proving
+    expect(checkForShip(player, [0, 0])).to.be.true;
+    expect(checkForShip(player, [0, 1])).to.be.true;
+    expect(checkForShip(player, [1, 0])).to.be.true;
+    expect(checkForShip(player, [1, 1])).to.be.true;
+    expect(checkForShip(player, [2, 3])).to.be.true;
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
+});
---
+++ test/test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+// test suite
+describe('Mocha', function(){
+  // test spec
+  it('should run our tests using npm', function(){
+    expect(true).to.be.ok;
+  });
+});
