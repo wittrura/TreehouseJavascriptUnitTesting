54fff105ed133ab3244e639e958ba3dbb22c85ad
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-05-14 19:06:12 -0400

    Adds assertion to test if ship is placed without direction

--- test/player_test.js
+++ test/player_test.js
@@ -96,5 +96,15 @@ describe('PLAYER METHODS', function () {
       expect(actual).to.have.length(1);
       expect(actual[0]).to.deep.equal([0, 1]);
     });
+
+    it('should throw an error if no direction is specified', function () {
+      var ship = player.ships[0];
+      var coordinates = [0, 1];
+      var handler = function () {
+        placeShip(player, ship, coordinates);
+      };
+
+      expect(handler).to.throw(Error);
+    });
   });
 });
