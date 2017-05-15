f8648cf56bbba3065fa7f7532c0f9adcb38b9e87
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-05-14 20:14:26 -0400

    Add fire and place ship methods to player logic for computer to randomly select valid values

--- game_logic/player_methods.js
+++ game_logic/player_methods.js
@@ -47,8 +47,30 @@ function placeShip (player, ship, startingCoordinates, direction) {
   }
 }
 
+function computerFire (player) {
+    var x = Math.floor(Math.random() * 9);
+    var y = Math.floor(Math.random() * 9);
+    var coordinates = [x, y];
+
+    fire(player, coordinates);
+}
+
+function computerPlaceShip (player, ship) {
+  var direction = Math.random() > 0.5
+    ? 'horizontal'
+    : 'vertical';
+
+  var x = Math.floor(Math.random() * 9);
+  var y = Math.floor(Math.random() * 9);
+  var coordinates = [x, y];
+
+  placeShip(player, ship, coordinates, direction);
+}
+
 module.exports = {
   placeShip: placeShip,
   validateLocations: validateLocations,
   validateLocation: validateLocation,
+  computerFire: computerFire,
+  computerPlaceShip: computerPlaceShip
 };
