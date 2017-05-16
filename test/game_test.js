754ad778deb9e04bcbe8e222f14c6374cc205958
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-05-15 20:24:05 -0400

    Create game test file with pending specs

---
+++ test/game_test.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+
+// x in front of describe or it can flag for pending specs
+describe('GAME INSTANCE FUNCTIONS', function(){
+
+  describe('checkGameStatus', function(){
+    // pending test spec, does not include callback
+    it('should tell me when the game is over');
+  });
+});
