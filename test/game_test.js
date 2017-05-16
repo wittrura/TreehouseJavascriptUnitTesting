eeb4f5f34898190199ef2934ac989a1ac9664ab4
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-05-15 20:27:57 -0400

    Add test for checkGameStatus

--- test/game_test.js
+++ test/game_test.js
@@ -4,7 +4,22 @@ var expect = require('chai').expect;
 describe('GAME INSTANCE FUNCTIONS', function(){
 
   describe('checkGameStatus', function(){
+    var checkGameStatus = require('../game_logic/game_instance.js').checkGameStatus;
+
     // pending test spec, does not include callback
-    it('should tell me when the game is over');
+    it('should tell me when the game is over', function() {
+      var players = [
+        {
+          ships: [
+            {
+              locations: [[0,0]],
+              damage: [[0,0]]
+            }
+          ]
+        }
+      ];
+      var actual = checkGameStatus(players);
+      expect(actual).to.be.false;
+    });
   });
 });
