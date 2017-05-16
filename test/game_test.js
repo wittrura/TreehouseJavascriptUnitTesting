a593d269869ad1e1bc3ae6c21a5129faac056ec2
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-05-16 19:59:40 -0400

    Add asynchronous test with callback for saveGame

--- test/game_test.js
+++ test/game_test.js
@@ -46,4 +46,25 @@ describe('GAME INSTANCE FUNCTIONS', function(){
     });
   });
 
+  // mock database call
+  function saveGame (callback) {
+    setTimeout(function () {
+      callback();
+    }, 1000);
+  }
+
+  describe('saveGame', function() {
+
+    it('should update save status', function(done) {
+      var status = 'game not saved...';
+
+      saveGame(function () {
+          status = 'game saved!';
+          expect(status).to.equal('game saved!');
+          done();
+      });
+
+    });
+  });
+
 });
